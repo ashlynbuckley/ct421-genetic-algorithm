de1db8876d49fb65df1a38b6d19ef0173a60a1c6
Ashlyn Buckley <ashlynbuckleyemail3@gmail.com>
2026-02-02 20:29:35 +0000

    conversion to 2d list

--- src/genetic_algorithm/parse_input.py
+++ src/genetic_algorithm/parse_input.py
@@ -8,17 +8,35 @@ def parse_variables(filename):
         variable_line = f.readline()
         for char in variable_line:
             if not char.isspace():
-                print("char: ", int(char))
                 variables.append(int(char))
-    # print(variables)
     n_exams = variables[0]
     n_slots = variables[1]
     n_students = variables[2]
-    print(n_exams, n_slots, n_students)
+    generate_input_array(filename, n_exams, n_students)
+
+def generate_input_array(filename, n_exams, n_students):
+    with open(filename, "r") as f:
+        # For every student generate a new row with length = n_exams
+        input_matrix = [[0 for _ in range(n_exams)] for _ in range(n_students)]
+        # Skip the variables line
+        next(f)
+        for i, line in enumerate(f):
+            j = 0
+            for value in line:
+                if not value.isspace():
+                    if int(value) == 0:
+                        input_matrix[i][j] = False
+                    else:
+                        input_matrix[i][j] = True
+                    j+=1
+    for row in input_matrix:
+        print(*row)
+
 
 # ENTRY POINT FUNCTION
 def read_instance(filename):
     parse_variables(filename)
+#     NEXT: CREATE ARRAYS
 
 # === STEPS ===
 # read in file
