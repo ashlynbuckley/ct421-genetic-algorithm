c7d94304de18ae85421675e464bca5c4d2cfcff1
Ashlyn Buckley <ashlynbuckleyemail3@gmail.com>
2026-02-04 22:01:36 +0000

    finished selection

--- src/genetic_algorithm/main.py
+++ src/genetic_algorithm/main.py
@@ -2,38 +2,33 @@
 import init_population as init_pop
 import parse_input as pi
 from evaluate_fitness import evaluate_population
-from crossover import crossover
-from mutate import mutate
-
+from src.genetic_algorithm.crossover import crossover
+from src.genetic_algorithm.selection import selection_process
 
 # Fields
 input_file = "tinyexample.txt"
 population_size = 100
-selection_percentage = 0.2
+elite_percentage = 0.05
+t_size = 5
 
 # Parse input
-mutation_rate = 1
 n_exams, n_slots, n_students, input_array = pi.parse_variables(input_file)
 
 # Initialise populations
 population = init_pop.init_population(population_size, n_exams, n_slots)
-print(population)
+# print(population)
 
 # Fitness
 results = evaluate_population(population, input_array)
-results = evaluate_population(population, input_array, n_slots)
-print(results)
+# print(results)
 
 # Select chromosomes to continue with
-# selection_process(population, results, selection_percentage)
+next_generation = selection_process(population, results, elite_percentage, t_size)
 
 #Crossover
-crossover(population, results, selection_percentage)
-print(crossover(population, results, selection_percentage))
+# crossover(population, results, selection_percentage)
+# print(crossover(population, results, selection_percentage))
 
 # Mutate
 
 # Repeat?
-# Avalon Brathwaite
-offspring = crossover(population, results, selection_percentage)
-mutate(offspring, mutation_rate)
--- src/genetic_algorithm/selection.py
+++ src/genetic_algorithm/selection.py
@@ -1,10 +1,49 @@
-# Written by Ashlyn ❀⸜(˶´ ˘ `˶)⸝❀
+# Written by Ashlyn and Avalon ❀⸜(˶´ ˘ `˶)⸝❀
+import random
 
-def tournament_selection(population, results):
-    # survival of the fittest logic (in random subsets to vary what passes on)
+def generate_new_generation(parents, elites):
+    next_generation = parents + elites
+    return next_generation
 
-def select_elites(population, results):
-    # pick the best individuals of the gen and ensure they carry into the next
+# Survival of the fittest logic (in random subsets to vary what passes on)
+def tournament_selection(paired, n_parents, t_size):
+    selected = 0
+    parents = []
+    while selected != n_parents:
+        contenders = []
+        # Select contenders randomly (with replacement)
+        for _ in range(t_size):
+            rand_chromosome = paired[random.randint(0, len(paired) - 1)]
+            contenders.append(rand_chromosome)
+        # FIGHT!
+        contenders.sort(reverse=True, key=lambda x: x[0])
+        parents.append(contenders[0])
+        selected += 1
+    return parents
 
-def selection_process(population, results, selection_percentage):
-    select_elites(population, results)
+# Pick the best individuals of the gen and ensure they carry into the next
+def select_elites(paired, elite_percentage):
+    # Select top % (number may vary)
+    n_elites = int(elite_percentage * len(paired))
+    # Append them to a separate list
+    elites = []
+    for _ in range(n_elites):
+        # Append the timetables of the elite solutions, remove them from the main pool
+        elites.append(paired.pop(0))
+    return elites
+
+def pair_results_chromosomes(population, results):
+    paired = []
+    # Create tuples of population with associated result
+    for i in range(len(population)):
+        paired.append((results[i], population[i]))
+    # Sort descending
+    paired.sort(reverse=True, key=lambda x: x[0])
+    return paired
+
+def selection_process(population, results, elite_percentage, t_size):
+    paired = pair_results_chromosomes(population, results)
+    elites = select_elites(paired, elite_percentage)
+    n_parents = len(paired)
+    selected = tournament_selection(paired, n_parents, t_size)
+    next_generation = generate_new_generation(selected, elites)
