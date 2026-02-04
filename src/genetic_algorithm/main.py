967d8de2459855e1358af67b1b632ab64904b7b7
Ashlyn Buckley <ashlynbuckleyemail3@gmail.com>
2026-02-04 20:26:50 +0000

    added basic code, added comments to main
    # Conflicts:
    #	.idea/misc.xml
    #	src/genetic_algorithm/main.py

--- src/genetic_algorithm/main.py
+++ src/genetic_algorithm/main.py
@@ -6,15 +6,34 @@ from crossover import crossover
 from mutate import mutate
 
 
+# Fields
 input_file = "tinyexample.txt"
 population_size = 100
 selection_percentage = 0.2
+
+# Parse input
 mutation_rate = 1
 n_exams, n_slots, n_students, input_array = pi.parse_variables(input_file)
+
+# Initialise populations
 population = init_pop.init_population(population_size, n_exams, n_slots)
 print(population)
+
+# Fitness
+results = evaluate_population(population, input_array)
 results = evaluate_population(population, input_array, n_slots)
 print(results)
+
+# Select chromosomes to continue with
+# selection_process(population, results, selection_percentage)
+
+#Crossover
+crossover(population, results, selection_percentage)
+print(crossover(population, results, selection_percentage))
+
+# Mutate
+
+# Repeat?
 # Avalon Brathwaite
 offspring = crossover(population, results, selection_percentage)
 mutate(offspring, mutation_rate)
---
+++ src/genetic_algorithm/selection.py
@@ -0,0 +1,10 @@
+# Written by Ashlyn ❀⸜(˶´ ˘ `˶)⸝❀
+
+def tournament_selection(population, results):
+    # survival of the fittest logic (in random subsets to vary what passes on)
+
+def select_elites(population, results):
+    # pick the best individuals of the gen and ensure they carry into the next
+
+def selection_process(population, results, selection_percentage):
+    select_elites(population, results)
