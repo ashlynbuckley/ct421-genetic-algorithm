7b485c628d7a0d9bb23fa0f141223b665a872b4b
Avalon Brathwaite <virlinthejammer@gmail.com>
2026-02-04 22:22:55 +0000

    Discrete cross over implemented

--- src/genetic_algorithm/main.py
+++ src/genetic_algorithm/main.py
@@ -2,7 +2,8 @@
 import init_population as init_pop
 import parse_input as pi
 from evaluate_fitness import evaluate_population
-from src.genetic_algorithm.crossover import crossover
+from crossover import crossover
+
 
 input_file = "tinyexample.txt"
 population_size = 100
@@ -12,5 +13,6 @@ population = init_pop.init_population(population_size, n_exams, n_slots)
 print(population)
 results = evaluate_population(population, input_array)
 print(results)
+
+# Avalon Brathwaite
 crossover(population, results, selection_percentage)
-print(crossover(population, results, selection_percentage))
