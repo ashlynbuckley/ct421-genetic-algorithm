e7ea1384cf0b913752e0e2ed1c8e823cf18fc0af
Ashlyn Buckley <ashlynbuckleyemail3@gmail.com>
2026-02-05 19:09:42 +0000

    new gen returned in main and given to crossover

--- src/genetic_algorithm/main.py
+++ src/genetic_algorithm/main.py
@@ -3,6 +3,7 @@ import init_population as init_pop
 import parse_input as pi
 from evaluate_fitness import evaluate_population
 from src.genetic_algorithm.crossover import crossover
+from mutate import mutate
 from src.genetic_algorithm.selection import selection_process
 
 # Fields
@@ -10,6 +11,7 @@ input_file = "tinyexample.txt"
 population_size = 100
 elite_percentage = 0.05
 t_size = 5
+mutation_rate = 1
 
 # Parse input
 n_exams, n_slots, n_students, input_array = pi.parse_variables(input_file)
@@ -19,16 +21,15 @@ population = init_pop.init_population(population_size, n_exams, n_slots)
 # print(population)
 
 # Fitness
-results = evaluate_population(population, input_array)
+results = evaluate_population(population, input_array, n_slots)
 # print(results)
 
 # Select chromosomes to continue with
 next_generation = selection_process(population, results, elite_percentage, t_size)
 
-#Crossover
-# crossover(population, results, selection_percentage)
-# print(crossover(population, results, selection_percentage))
-
-# Mutate
+# Crossover & Mutate
+# Avalon Brathwaite
+offspring = crossover(next_generation, results, selection_percentage)
+# mutate(offspring, mutation_rate)
 
 # Repeat?
