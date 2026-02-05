af7c4112b72e6c41edd7c708717ba285e7421475
Avalon Brathwaite <virlinthejammer@gmail.com>
2026-02-05 13:50:40 +0000

    Implemented mutate function

--- src/genetic_algorithm/main.py
+++ src/genetic_algorithm/main.py
@@ -3,11 +3,13 @@ import init_population as init_pop
 import parse_input as pi
 from evaluate_fitness import evaluate_population
 from crossover import crossover
+from mutate import mutate
 
 
 input_file = "tinyexample.txt"
 population_size = 100
 selection_percentage = 0.2
+mutation_rate = 1
 n_exams, n_slots, n_students, input_array = pi.parse_variables(input_file)
 population = init_pop.init_population(population_size, n_exams, n_slots)
 print(population)
@@ -15,4 +17,5 @@ results = evaluate_population(population, input_array)
 print(results)
 
 # Avalon Brathwaite
-crossover(population, results, selection_percentage)
+offspring = crossover(population, results, selection_percentage)
+mutate(offspring, mutation_rate)
---
+++ src/genetic_algorithm/mutate.py
@@ -0,0 +1,37 @@
+# Written by Avalon Brathwaite
+import random
+
+def mutate(offspring, mutation_rate):
+    mutated_offspring = []
+
+    for timetable in offspring:
+        timetable = [row[:] for row in timetable] # Avoid mutating original references
+
+        if random.random() < mutation_rate:
+            # Pick a random exam row within the timetable
+            exam = random.randrange(len(timetable))
+            slots = timetable[exam] # Gives us all the time slots for the random exam row chosen
+
+            print("Unmutated Offspring row: ", slots)
+
+            scheduled_exam_slot = slots.index(True)
+            new_slot = random.randrange(len(slots))
+
+           # Always pick a different slot
+            while new_slot == scheduled_exam_slot:
+                new_slot = random.randrange(len(slots))
+
+            # Shift the current true exam slot to avoid violating constraints, this could give us consecutive exams or no exams scheduled at all.
+            slots[scheduled_exam_slot] = False
+            slots[new_slot] = True
+            print("Mutated Offspring: ", slots)
+
+            mutated_offspring.append(timetable)
+
+    return mutated_offspring
+
+# Visual Help >:D timetable =
+#     [False, True,  False],  # exam 0
+#     [True,  False, False],  # exam 1
+#     [False, False, True ],  # exam 2
+#     [False, True,  False]   # exam 3
