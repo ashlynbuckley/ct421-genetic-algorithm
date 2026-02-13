140eae7769366f7bf0b6023a55d767e4678afee2
saphyzzz <virlinthejammer@gmail.com>
2026-02-13 15:22:38 +0000

    Iterating through generations on main and makes two children instead of one

--- src/genetic_algorithm/main.py
+++ src/genetic_algorithm/main.py
@@ -1,4 +1,3 @@
-# Written by Karolina
 import init_population as init_pop
 from evaluate_fitness import evaluate_population
 from crossover import crossover
@@ -8,11 +7,11 @@ from src.genetic_algorithm.selection import selection_process
 
 # Fields
 input_file = "tinyexample.txt"
-population_size = 1000
+population_size = 100
 elite_percentage = 0.05
 t_size = 5
 mutation_rate = 1
-generations = 500
+generations = 10
 
 # Parse input
 n_exams, n_slots, n_students, input_array = read_input_file(input_file)
@@ -30,7 +29,6 @@ for i in range(generations):
     next_generation = selection_process(population, results, elite_percentage, t_size)
 
     # Crossover & Mutate
-    # Avalon Brathwaite
     offspring = crossover(next_generation)
     mutated_offspring = mutate(offspring, mutation_rate)
 
